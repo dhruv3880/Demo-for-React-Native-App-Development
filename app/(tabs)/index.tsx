10277726638cce46ca5b5640a4c01c2194f76f08
Dhruv <dv160603@gmail.com>
2024-12-20 13:09:48 +0530

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,59 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
-export default function Page() {
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+
+
+const placeholderImg = require('@/assets/images/profile.jpg');
+
+export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+    else {
+      alert("You didn't choose the image");
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        {/* <Link href='/about'>hi go to about</Link> */}
-        <Text>Hii</Text>
+      <View style={styles.imagecontainer}>
+        <ImageViewer imgSource={placeholderImg} selectedImage={selectedImage} />
+      </View>
+      <View style={styles.footercontainer}>
+        <Button theme="primary" label="Choose Image" onPress={pickImageAsync} />
+        <Button label="Use this image" />
       </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
+    backgroundColor: 'lightgreen',
+    alignItems: 'center'
   },
-  main: {
+  imagecontainer: {
     flex: 1,
-    justifyContent: "center",
+    alignItems: 'center'
+  },
+  footercontainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
