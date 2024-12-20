e68a3ed91455c37b26f02d8bc158192c9095eb70
Dhruv <dv160603@gmail.com>
2024-12-20 16:36:53 +0530

    update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
-
+import { useEffect, useRef, useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import CircleButton from "@/components/CircleButton";
@@ -10,10 +12,20 @@ import IconButton from "@/components/IconButton";
 const placeholderImg = require('@/assets/images/profile.jpg');
 
 export default function Index() {
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
   const [showAppOtpions, setShowAppOptions] = useState<boolean>(false);
 
-  const pickImageAsync = async () => {
+  useEffect(() => {
+    if (!status?.granted) {
+      requestPermission();
+    }
+  }, [])
+
+
+
+  async function pickImageAsync() {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
@@ -27,7 +39,7 @@ export default function Index() {
     else {
       alert("You didn't choose the image");
     }
-  };
+  }
 
   const onReset = () => {
     setShowAppOptions(false);
@@ -37,16 +49,49 @@ export default function Index() {
 
   }
 
-  const onSaveImageAsync = () => {
+  const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
 
-  }
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('saved!');
+        }
+
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current,{
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+  };
 
 
 
   return (
     <View style={styles.container}>
       <View style={styles.imagecontainer}>
-        <ImageViewer imgSource={placeholderImg} selectedImage={selectedImage} />
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer imgSource={placeholderImg} selectedImage={selectedImage} />
+        </View>
       </View>
       {showAppOtpions ? (
         <View style={styles.optionsContainer}>
@@ -59,11 +104,13 @@ export default function Index() {
       ) : (
         <View style={styles.footercontainer}>
           <Button theme="primary" label="Choose Image" onPress={pickImageAsync} />
-          <Button label="Use this image" />
+          <Button label="Use this image" onPress={() => setShowAppOptions(true)} />
         </View>
       )}
     </View>
   );
+
+
 }
 
 
